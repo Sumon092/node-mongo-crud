56b04b751cd56fbc5447f24f3a7e0bd4b71c9959
sumon092 <m.sumon@outlook.com>
2022-04-23 15:16:47 +0600

    mongo db added

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//use middleware
+
+
+// usre: dbuser1
+//password: vYDQRt8xhJ1PLzfc
+
+
+
+
+const uri = "mongodb+srv://dbuser1:vYDQRt8xhJ1PLzfc@cluster0.zwxug.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        usersColloection = client.db('foodExpress').collection("user");
+        const user = { name: 'Mahiya Mahi', email: 'mahi@gmail.com' };
+        const result = await usersColloection.insertOne(user);
+        console.log(`user inserted with id: ${result.insertedId}`);
+    }
+    finally {
+        //await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Running my CRUD Server');
+})
+
+app.listen(port, () => {
+    console.log('CRUD Server is running');
+})
